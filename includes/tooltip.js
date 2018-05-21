f3260bbff0c90ce675c993319317177622cf203d
HySteffensen <hyrum.steffensen@gmail.com>
2018-05-20 19:40:22 -0700

    04_01 Attaching tip container to the mouse

--- includes/tooltip.js
+++ includes/tooltip.js
@@ -3,11 +3,23 @@
 
 $(document).ready(function() {
     $('.tooltip').mouseover(function(e){
+
         if( $(this).attr('data-tip-type') == 'text' ) {
             $('#tooltip_container').html( $(this).attr('data-tip-source') );
         }
-    }).mousemove(function(e){
 
+        if( $(this).attr('data-tip-type') == 'html' ) {
+            var elementToGet = '#' + $(this).attr('data-tip-source');
+            var newHTML = $(elementToGet).html();
+            $('#tooltip_container').html(newHTML);
+        }
+
+    }).mousemove(function(e){
+        var toolTipWidth = $('#tooltip_container').outerWidth();
+        var toolTipHeight = $('#tooltip_container').outerHeight();
+
+        $('#tooltip_container').css('left', (e.pageX - 20) + 'px');
+        $('#tooltip_container').css('top', (e.pageY + 20) + 'px');
     }).mouseout(function(e){
 
     });
