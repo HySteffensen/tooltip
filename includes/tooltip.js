0f76183ffa4d8fccae1323e196b0a54b2d756e5d
HySteffensen <hyrum.steffensen@gmail.com>
2018-05-20 17:12:55 -0700

    04_03 created mouseover event for tooltip

--- includes/tooltip.js
+++ includes/tooltip.js
@@ -1,7 +1,17 @@
 /*  JavaScript Document                      */
 /*  Written by Chris Converse for lynda.com  */
 
+$(document).ready(function() {
+    $('.tooltip').mouseover(function(e){
+        if( $(this).attr('data-tip-type') == 'text' ) {
+            $('#tooltip_container').html( $(this).attr('data-tip-source') );
+        }
+    }).mousemove(function(e){
 
+    }).mouseout(function(e){
+
+    });
+});
 
 
 
